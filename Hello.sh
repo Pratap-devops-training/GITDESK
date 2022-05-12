5393f8153ee8dca24d6a53775c3b45e4984cf9ed
Pratap2222 <dsp.eee222@gmail.com>
2022-05-12 17:44:44 +0530

    Re initiated file

--- Hello.sh
+++ Hello.sh
@@ -1,6 +1,1 @@
-echo "First Script in Windows server after installing Portable Git "
-echo "Test line added as second commiti"
-echo "Adding a newline in master branch"
-echo "Test line added as second commit"
-# this line update in Dev Branch
-
+echo "Greetings of the day "
